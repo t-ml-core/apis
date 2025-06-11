b25ad6896cf9f7bc2d9f532ad7ec3311244c10c3
Pavel Kuzmuk <p.kuzmuk@tbank.ru>
2025-06-11 16:50:55 +0300

    Shared limit queue group annotation

--- pkg/apis/scheduling/v1beta1/labels.go
+++ pkg/apis/scheduling/v1beta1/labels.go
@@ -70,3 +70,11 @@ const PodQosLevel = "volcano.sh/qos-level"
 
 // EnableInQuotasAnnotationKey is the key of enable in quotas plugin, true or false
 const EnableInQuotasAnnotationKey = "volcano.sh/enable-in-quotas-plugin"
+
+// SharedLimitGroupKey is a key to determine queue relation to
+// shared limit group
+const SharedLimitGroupKey = "volcano.sh/shared-limit-group-key"
+
+// IsSharedLimitGroupLeader is true if queue's limits should be interpreted as limits
+// for all jobs in shared limit group
+const IsSharedLimitGroupLeader = "volcano.sh/is-shared-limit-group-leader"
