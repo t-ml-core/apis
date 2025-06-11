4236c07aca73689ab3842a3d7b7a3a54e16b67d1
Pavel <pasha.kuzmuk@gmail.com>
2025-06-11 17:28:29 +0300

    Add shared limits queue group annotations
    ---------
    
    Co-authored-by: Pavel Kuzmuk <p.kuzmuk@tbank.ru>

--- pkg/apis/scheduling/v1beta1/labels.go
+++ pkg/apis/scheduling/v1beta1/labels.go
@@ -70,3 +70,11 @@ const PodQosLevel = "volcano.sh/qos-level"
 
 // EnableInQuotasAnnotationKey is the key of enable in quotas plugin, true or false
 const EnableInQuotasAnnotationKey = "volcano.sh/enable-in-quotas-plugin"
+
+// SharedLimitsGroupKey is a key to determine queue relation to
+// shared limits group
+const SharedLimitsGroupKey = "volcano.sh/shared-limits-group-key"
+
+// IsSharedLimitsGroupLeader is true if queue's limits should be interpreted as limits
+// for all jobs in shared limits group
+const IsSharedLimitsGroupLeader = "volcano.sh/is-shared-limits-group-leader"
